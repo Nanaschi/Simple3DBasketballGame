79136dde096ddc26ae35f3eed899afef23cdb475
Nanaschi <naturforscher@outlook.com>
2021-01-13 02:38:39 +0700

    0.1

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public GameObject trailObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trailObject.SetActive (false); //the trail does not happen
+    }
+    void Update (){
+      if (Input.GetKeyDown(KeyCode.R)){
+        GetComponent<Renderer>().material.color = Color.blue; 
+      }
+       if (Input.GetKeyDown(KeyCode.T)){
+        GetComponent<Renderer>().material.color = Color.black; 
+      }
+       if (Input.GetKeyDown(KeyCode.Y)){
+        GetComponent<Renderer>().material.color = Color.green; 
+      }
+       if (Input.GetKeyDown(KeyCode.U)){
+        GetComponent<Renderer>().material.color = Color.yellow; 
+      }
+    }
+
+  public void ActivateTrail(){
+  trailObject.SetActive (true); //the trail appears
+  }
+}
---
+++ Assets/Script/Radio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Radio : MonoBehaviour
+ {
+   
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("The radio touches the " + collision.gameObject.name);
+
+        if (collision.gameObject.name == "Ball")
+        {
+            GameObject.Find("Music").GetComponent<AudioSource>().Pause();
+        } 
+
+    }
+    
+
+}
+
